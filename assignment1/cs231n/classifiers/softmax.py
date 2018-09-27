25350abf17b44e0c676eec15e244d3c0fa689a3e
sherlock <sherlockliao01@gmail.com>
2018-09-27 23:40:27 +0800

    finish softmax
    finish softmax

--- assignment1/cs231n/classifiers/softmax.py
+++ assignment1/cs231n/classifiers/softmax.py
@@ -29,7 +29,24 @@ def softmax_loss_naive(W, X, y, reg):
     # here, it is easy to run into numeric instability. Don't forget the        #
     # regularization!                                                           #
     #############################################################################
-    pass
+    for i in range(X.shape[0]):
+        score = np.dot(X[i], W)
+        score -= max(score)  # 为了数值稳定性
+        score = np.exp(score)  # 取指数
+        softmax_sum = np.sum(score)  # 得到分母
+        score /= softmax_sum  # 除以分母得到softmax
+        # 计算梯度
+        for j in range(W.shape[1]):
+            if j != y[i]:
+                dW[:, j] += score[j] * X[i]
+            else:
+                dW[:, j] -= (1 - score[j]) * X[i]
+
+        loss -= np.log(score[y[i]])  # 得到交叉熵
+    loss /= X.shape[0]  # 平均
+    dW /= X.shape[0]  # 平均
+    loss += reg * np.sum(W * W)  # 加上正则项
+    dW += 2 * reg * W
     #############################################################################
     #                          END OF YOUR CODE                                 #
     #############################################################################
@@ -53,7 +70,19 @@ def softmax_loss_vectorized(W, X, y, reg):
     # here, it is easy to run into numeric instability. Don't forget the        #
     # regularization!                                                           #
     #############################################################################
-    pass
+    scores = np.dot(X, W)
+    scores -= np.max(scores, axis=1, keepdims=True)  # 数值稳定性
+    scores = np.exp(scores)
+    scores /= np.sum(scores, axis=1, keepdims=True)  # softmax
+    ds = np.copy(scores)
+    ds[np.arange(X.shape[0]), y] -= 1
+    dW = np.dot(X.T, ds)
+    loss = scores[np.arange(X.shape[0]), y]
+    loss = -np.log(loss).sum()
+    loss /= X.shape[0]
+    dW /= X.shape[0]
+    loss += reg * np.sum(W * W)
+    dW += 2 * reg * W
     #############################################################################
     #                          END OF YOUR CODE                                 #
     #############################################################################
